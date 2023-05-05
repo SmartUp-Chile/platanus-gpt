3e92a70b211d6c43968bb156d7302c665663638a
Daniel <satelerd@gmail.com>
2023-05-05 10:47:05 -0400

    Add timestamps to the json

--- scripts/scrape.ts
+++ scripts/scrape.ts
@@ -10,14 +10,22 @@ import { readFile } from "fs/promises";
 
 const CHUNK_SIZE = 200;
 const TRANSCRIPTIONS_DIR = "./scripts/data"; // Reemplaza esto con la ruta de tu carpeta de transcripciones
+const videoUrl = "https://youtu.be/wZ-VucSOZus";
 
-
+// Aux functions
 function convertToUTF8(input: string): string {
   const buffer = iconv.encode(input, "ISO-8859-1");
   return iconv.decode(buffer, "utf-8");
 }
 
+function timeToSeconds(time: string | undefined): number {
+  if (typeof time !== 'string') return 0;
+  const [hours, minutes, seconds] = time.split(':').map(Number);
+  return hours * 3600 + minutes * 60 + seconds;
+}
 
+
+// Important functions
 const readExcelFile = async (filename: string) => {
   const buffer = await readFile(filename);
   const utf8Buffer = iconv.decode(buffer, "ISO-8859-1");
@@ -35,14 +43,13 @@ const readExcelFile = async (filename: string) => {
   return data;
 };
 
-
 const getTranscription = async (data: any[][]) => {
   const content = data
     .map((row) => row.join(","))
     .join("\r\n");
 
   let transcription: PGEssay = {
-    title: "Nombre del título", // Reemplaza esto con el título que desees
+    title: "Platanus - Cómo hacer una buena postulación a PV",
     url: "",
     date: "",
     thanks: "",
@@ -55,41 +62,48 @@ const getTranscription = async (data: any[][]) => {
   return transcription;
 };
 
-
 const chunkTranscription = async (transcription: PGEssay, data: any[][]) => {
   console.log("chunking", transcription.title);
   const { title, url, date, thanks, content, ...chunklessSection } = transcription;
 
   let transcriptionTextChunks = [];
   let chunkText = "";
+  let startTime = 0;
 
   for (let i = 1; i < data.length; i++) { // Comienza en 1 para ignorar los encabezados
     const row = data[i];
     const speaker = row[3];
     const sentence = convertToUTF8(row[4]);
-    console.log("chunking", i, sentence)
 
     const sentenceTokenLength = encode(sentence).length;
     const chunkTextTokenLength = encode(chunkText).length;
 
     if (chunkTextTokenLength + sentenceTokenLength > CHUNK_SIZE || (i > 1 && speaker !== data[i - 1][3])) {
-      transcriptionTextChunks.push(chunkText);
+      transcriptionTextChunks.push({ text: chunkText, startTime });
+      startTime = row[1];
       chunkText = "";
     }
 
     chunkText += sentence + " ";
   }
 
-  transcriptionTextChunks.push(chunkText.trim());
+  transcriptionTextChunks.push({ text: chunkText.trim(), startTime });
 
-  const transcriptionChunks = transcriptionTextChunks.map((text) => {
+  const transcriptionChunks = transcriptionTextChunks.map((text, index) => {
     const trimmedText = text.trim();
 
+    // Buscamos el índice del texto en data y obtenemos el speaker correspondiente
+    const rowIndex = data.findIndex(row => row[4] === text);
+    const speaker = rowIndex !== -1 ? data[rowIndex][3] : '';
+    const speakerName = rowIndex !== -1 ? data[rowIndex][2] : '';
+
+    const startTimeInSeconds = rowIndex !== -1 ? timeToSeconds(data[rowIndex][0]) : 0;
+
     const chunk: PGChunk = {
-      essay_title: title,
-      essay_url: url,
-      essay_date: date,
-      essay_thanks: thanks,
+      essay_title: "Platanus - Cómo hacer una buena postulación a PV",
+      essay_url: `https://www.youtube.com/watch?v=wZ-VucSOZus&feature=youtu.be&t=${startTimeInSeconds}`,
+      essay_date: "",
+      essay_thanks: speakerName,
       content: trimmedText,
       content_length: trimmedText.length,
       content_tokens: encode(trimmedText).length,
@@ -99,11 +113,13 @@ const chunkTranscription = async (transcription: PGEssay, data: any[][]) => {
     return chunk;
   });
 
+
+
+
   if (transcriptionChunks.length > 1) {
     for (let i = 0; i < transcriptionChunks.length; i++) {
       const chunk = transcriptionChunks[i];
       const prevChunk = transcriptionChunks[i - 1];
-
       if (chunk.content_tokens < 100 && prevChunk) {
         prevChunk.content += " " + chunk.content;
         prevChunk.content_length += chunk.content_length;
@@ -121,7 +137,6 @@ const chunkTranscription = async (transcription: PGEssay, data: any[][]) => {
 
   return chunkedSection;
 };
-
 
 (async () => {
   const data = await readExcelFile("./scripts/data/como_postular_platanus.csv"); // Reemplaza esto con la ruta de tu archivo de Excel
